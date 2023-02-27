4e5256401cc01f589e81fd78a506a6668ce90aa1
Seiya Terada <54200250+seiyaterada@users.noreply.github.com>
2023-02-26 16:45:32 -0800

    y

--- src/main.c
+++ src/main.c
@@ -1,27 +1,21 @@
 /*
     FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
     All rights reserved
-
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
-
     This file is part of the FreeRTOS distribution.
-
     FreeRTOS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License (version 2) as published by the
     Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
-
     ***************************************************************************
     >>!   NOTE: The modification to the GPL is included to allow you to     !<<
     >>!   distribute a combined work that includes FreeRTOS without being   !<<
     >>!   obliged to provide the source code for proprietary components     !<<
     >>!   outside of the FreeRTOS kernel.                                   !<<
     ***************************************************************************
-
     FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
     FOR A PARTICULAR PURPOSE.  Full license text is available on the following
     link: http://www.freertos.org/a00114.html
-
     ***************************************************************************
      *                                                                       *
      *    FreeRTOS provides completely free yet professionally developed,    *
@@ -35,35 +29,27 @@
      *    http://www.FreeRTOS.org/Documentation                              *
      *                                                                       *
     ***************************************************************************
-
     http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
     the FAQ page "My application does not run, what could be wwrong?".  Have you
     defined configASSERT()?
-
     http://www.FreeRTOS.org/support - In return for receiving this top quality
     embedded software for free we request you assist our global community by
     participating in the support forum.
-
     http://www.FreeRTOS.org/training - Investing in training allows your team to
     be as productive as possible as early as possible.  Now you can receive
     FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
     Ltd, and the world's leading authority on the world's leading RTOS.
-
     http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
     including FreeRTOS+Trace - an indispensable productivity tool, a DOS
     compatible FAT file system, and our tiny thread aware UDP/IP stack.
-
     http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
     Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
-
     http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
     Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
     licenses offer ticketed support, indemnification and commercial middleware.
-
     http://www.SafeRTOS.com - High Integrity Systems also provide a safety
     engineered and independently SIL3 certified version for use in safety and
     mission critical applications that require provable dependability.
-
     1 tab == 4 spaces!
 */
 
@@ -73,22 +59,17 @@ FreeRTOS is a market leading RTOS from Real Time Engineers Ltd. that supports
 developed, strictly quality controlled, robust, supported, and free to use in
 commercial products without any requirement to expose your proprietary source
 code.
-
 This simple FreeRTOS demo does not make use of any IO ports, so will execute on
 any Cortex-M3 of Cortex-M4 hardware.  Look for TODO markers in the code for
 locations that may require tailoring to, for example, include a manufacturer
 specific header file.
-
 This is a starter project, so only a subset of the RTOS features are
 demonstrated.  Ample source comments are provided, along with web links to
 relevant pages on the http://www.FreeRTOS.org site.
-
 Here is a description of the project's functionality:
-
 The main() Function:
 main() creates the tasks and software timers described in this section, before
 starting the scheduler.
-
 The Queue Send Task:
 The queue send task is implemented by the prvQueueSendTask() function.
 The task uses the FreeRTOS vTaskDelayUntil() and xQueueSend() API functions to
@@ -96,7 +77,6 @@ periodically send the number 100 on a queue.  The period is set to 200ms.  See
 the comments in the function for more details.
 http://www.freertos.org/vtaskdelayuntil.html
 http://www.freertos.org/a00117.html
-
 The Queue Receive Task:
 The queue receive task is implemented by the prvQueueReceiveTask() function.
 The task uses the FreeRTOS xQueueReceive() API function to receive values from
@@ -105,30 +85,25 @@ receive task increments the ulCountOfItemsReceivedOnQueue variable each time it
 receives the value 100.  Therefore, as values are sent to the queue every 200ms,
 the value of ulCountOfItemsReceivedOnQueue will increase by 5 every second.
 http://www.freertos.org/a00118.html
-
 An example software timer:
 A software timer is created with an auto reloading period of 1000ms.  The
 timer's callback function increments the ulCountOfTimerCallbackExecutions
 variable each time it is called.  Therefore the value of
 ulCountOfTimerCallbackExecutions will count seconds.
 http://www.freertos.org/RTOS-software-timer.html
-
 The FreeRTOS RTOS tick hook (or callback) function:
 The tick hook function executes in the context of the FreeRTOS tick interrupt.
 The function 'gives' a semaphore every 500th time it executes.  The semaphore
 is used to synchronise with the event semaphore task, which is described next.
-
 The event semaphore task:
 The event semaphore task uses the FreeRTOS xSemaphoreTake() API function to
 wait for the semaphore that is given by the RTOS tick hook function.  The task
 increments the ulCountOfReceivedSemaphores variable each time the semaphore is
 received.  As the semaphore is given every 500ms (assuming a tick frequency of
 1KHz), the value of ulCountOfReceivedSemaphores will increase by 2 each second.
-
 The idle hook (or callback) function:
 The idle hook function queries the amount of free FreeRTOS heap space available.
 See vApplicationIdleHook().
-
 The malloc failed and stack overflow hook (or callback) functions:
 These two hook functions are provided as examples, but do not contain any
 functionality.
@@ -150,14 +125,23 @@ functionality.
 
 /*-----------------------------------------------------------*/
 #define mainQUEUE_LENGTH 100
+#define TRAFFIC_GREEN GPIO_Pin_0
+#define TRAFFIC_AMBER GPIO_Pin_1
+#define TRAFFIC_RED GPIO_Pin_2
+
+#define NEW_CAR 0x80000000
 
 /*
  * TODO: Implement this function for any hardware specific clock configuration
  * that was not already performed before main() was called.
  */
 static xQueueHandle xQueue = NULL;
+static xQueueHandle xFlowQueue = NULL;
 static void prvSetupHardware( void );
 static void prvDisplayBoard( void *pvParameters );
+static void prvCarTraffic( void *pvParameters );
+static void prvCarTrafficCreator( void *pvParameters );
+static void prvTrafficFlow( void *pvParameters );
 
 /*
  * The queue send and receive tasks as described in the comments at the top of
@@ -175,6 +159,17 @@ void writeBoard( uint32_t value )
 		}
 }
 
+uint16_t get_ADC_Value() {
+	uint16_t adc_value;
+	ADC_SoftwareStartConv(ADC1);
+	while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC));
+	adc_value = ADC_GetConversionValue(ADC1)/1024;
+
+	if(adc_value == 4) adc_value = 3;
+
+	return adc_value;
+}
+
 /*-----------------------------------------------------------*/
 
 int main(void)
@@ -182,12 +177,74 @@ int main(void)
 	prvSetupHardware();
 	xQueue = xQueueCreate( 	mainQUEUE_LENGTH,			/* The number of items the queue can hold. */
 								sizeof( uint32_t ) );		/* The size of each item the queue holds. */
+	xFlowQueue = xQueueCreate( 	mainQUEUE_LENGTH,			/* The number of items the queue can hold. */
+									sizeof( uint16_t ) );		/* The size of each item the queue holds. */
 	vQueueAddToRegistry( xQueue, "MainQueue" );
+	vQueueAddToRegistry( xFlowQueue, "MainQueue" );
 	u_int32_t defaultBoardState = (0x80000000);
 	xQueueSend( xQueue, &defaultBoardState, 0);
 	xTaskCreate( prvDisplayBoard, "DisplayBoardTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate( prvCarTraffic, "CarTraffic", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+//	xTaskCreate( prvCarTrafficCreator, "CarTrafficCreator", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+//	xTaskCreate( prvTrafficFlow, "TrafficFlow", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 	vTaskStartScheduler();
 	return 0;
+}
+
+static void prvCarTraffic(void *pvParameters) {
+	uint32_t boardstate;
+
+	while(1) {
+		vTaskDelay(500);
+		xQueueReceive(xQueue, &boardstate, portMAX_DELAY);
+
+		boardstate = 0x00000000 | (boardstate >> 1);
+		xQueueSend( xQueue, &boardstate, 0);
+	}
+}
+
+static void prvCarTrafficCreator(void *pvParameters) {
+	uint16_t flow = 4;
+	uint16_t car_value = 0;
+	uint32_t boardstate;
+
+	while(1) {
+		vTaskDelay(500);
+		xQueueReceive(xFlowQueue, &flow, portMAX_DELAY);
+		xQueueReceive(xQueue, &boardstate, portMAX_DELAY);
+
+		int prob = rand() % 4;
+		//car_value = (rand() % 100) < 100/(4 - flow);
+		if(prob >= flow) {
+			boardstate = 0x00000000 | boardstate | 0x80000000;
+			car_value = 1;
+		} else {
+			car_value = 0;
+		}
+
+		xQueueSend(xFlowQueue, &flow, 0);
+		xQueueSend(xQueue, &boardstate, 0);
+	}
+}
+
+static void prvTrafficFlow(void *pvParameters) {
+	uint16_t flow_value = 0;
+	uint16_t current_flow_value = 0;
+
+	while(1) {
+		vTaskDelay(500);
+		flow_value = get_ADC_Value();
+
+		if(abs(current_flow_value - flow_value) != 0) {
+			current_flow_value = flow_value;
+
+			xQueueSend(xFlowQueue, &flow_value, portMAX_DELAY);
+		}
+
+
+//		xQueueReceive(xFlowQueue, &flow_value, portMAX_DELAY);
+
+	}
 }
 
 
@@ -197,17 +254,17 @@ static void prvDisplayBoard(void *pvParameters) {
 	while(1) {
 		// Run every 10mx
 		vTaskDelay(10);
+		GPIO_SetBits(GPIOD, TRAFFIC_GREEN);
+		GPIO_SetBits(GPIOD, TRAFFIC_AMBER);
+		GPIO_SetBits(GPIOD, TRAFFIC_RED);
+//		GPIO_SetBits(GPIOD, )
 
-//		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
+		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
 
 		// Write to board
-		writeBoard(0xFFFFFFFF);
-//		GPIO_Write(GPIOB, GPIO_Pin_0 & 0xFFFFFFFF);
-//		GPIO_ToggleBits(GPIOB, GPIO_Pin_1);
+		writeBoard(ulReceivedValue);
 
-
-
-//		xQueueSend( xQueue, &ulReceivedValue, 0);
+		xQueueSend( xQueue, &ulReceivedValue, 0);
 	}
 }
 
@@ -220,7 +277,6 @@ void vApplicationMallocFailedHook( void )
 {
 	/* The malloc failed hook is enabled by setting
 	configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
-
 	Called if a call to pvPortMalloc() fails because there is insufficient
 	free memory available in the FreeRTOS heap.  pvPortMalloc() is called
 	internally by FreeRTOS API functions that create tasks, queues, software
@@ -250,7 +306,6 @@ volatile size_t xFreeStackSpace;
 
 	/* The idle task hook is enabled by setting configUSE_IDLE_HOOK to 1 in
 	FreeRTOSConfig.h.
-
 	This function is called on each cycle of the idle task.  In this case it
 	does nothing useful, other than report the amount of FreeRTOS heap that
 	remains unallocated. */
@@ -290,7 +345,7 @@ static void prvSetupHardware( void )
 	GPIO_Init(GPIOB, &Shift1);
 
 	Traffic_Lights.GPIO_Mode = GPIO_Mode_OUT;
-	Traffic_Lights.GPIO_Pin = GPIO_Pin_2 ;
+	Traffic_Lights.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
 	Traffic_Lights.GPIO_OType = GPIO_OType_PP;
 	Traffic_Lights.GPIO_PuPd = GPIO_PuPd_DOWN;
 	GPIO_Init(GPIOD, &Traffic_Lights);
@@ -320,4 +375,3 @@ static void prvSetupHardware( void )
 
 	ADC_RegularChannelConfig(ADC1, ADC_Channel_1, 1, ADC_SampleTime_84Cycles);
 }
-
