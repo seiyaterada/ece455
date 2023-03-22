91d23e9e603b9f6a88241846fe64774babc55205
Seiya Terada <54200250+seiyaterada@users.noreply.github.com>
2023-03-21 18:01:58 -0700

    Update main.c

--- main.c
+++ main.c
@@ -370,7 +370,13 @@ static void DD_Scheduler( void *pvParameters )
 	}
 }
 
-static void Task_Generator( void *pvParameters )
+
+static void Monitor_Task( void *pvParameters )
+{
+	return 0;
+}
+
+static void Task_1_Generator( void *pvParameters )
 {
 	while (1) {
     TaskHandle_t task_handle;
@@ -399,11 +405,6 @@ static void Task_Generator( void *pvParameters )
   }
 }
 
-static void Monitor_Task( void *pvParameters )
-{
-	return 0;
-}
-
 void Periodic_Task_1(void *pvParameters) {
   TickType_t current_tick = xTaskGetTickCount();
   TickType_t previous_tick = 0;
@@ -419,6 +420,52 @@ void Periodic_Task_1(void *pvParameters) {
   }
   // There's no need to vTaskDelay here. If we're done we're done.
   delete_dd_task(1);
+}
+
+static void Task_2_Generator( void *pvParameters )
+{
+	while (1) {
+    TaskHandle_t task_handle;
+    xTaskCreate(Periodic_Task_2,          // TaskFunction_t Function
+                "Task_2",                 // const char *const pcName
+                configMINIMAL_STACK_SIZE, // configSTACK_DEPTH_TYPE usStackDepth
+                NULL,                     // void *const pvParameters
+                DD_PRIORITY_UNSCHEDULED,  // UBaseType_t uxPriority
+                &(task_handle)            // TaskHandle_t *const pxCreatedTask
+    );
+    if (task_handle == NULL) {
+      printf("Generator xTaskCreate task handle is NULL\n");
+      return;
+    }
+    // Let the DDS start it later with a new priority
+    vTaskSuspend(task_handle);
+
+    create_dd_task(
+        task_handle,                        // TaskHandle_t task_handle,
+        PERIODIC,                           // DD_Task_Enum_t type,
+        1,                                  // uint32_t id,
+        xTaskGetTickCount() + TASK_2_PERIOD // uint32_t absolute_deadline);
+    );
+    // Delay THIS generator task. Not the created one
+    vTaskDelay(TASK_2_PERIOD);
+  }
+}
+
+void Periodic_Task_2(void *pvParameters) {
+  TickType_t current_tick = xTaskGetTickCount();
+  TickType_t previous_tick = 0;
+  TickType_t execution_time = TASK_2_EXEC_TIME / portTICK_PERIOD_MS;
+//  printf("Task 1 (F-Task Priority: %u; Tick: %u)\n",
+//         (unsigned int)uxTaskPriorityGet(NULL), (unsigned int)current_tick);
+  while (execution_time) {
+    current_tick = xTaskGetTickCount();
+    if (current_tick == previous_tick)
+      continue;
+    previous_tick = current_tick;
+    execution_time--;
+  }
+  // There's no need to vTaskDelay here. If we're done we're done.
+  delete_dd_task(2);
 }
 
 
